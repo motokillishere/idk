f9c76814dc271ba4df8fb15a4c47ad7a5761b4ac
student <student@BTU.local>
2019-10-13 13:53:59 +0400

    first commit

---
+++ 8_October/main.js
@@ -0,0 +1,23 @@
+// ASI
+
+/**
+ * Comment
+ */
+
+ // var,
+ //let,const
+
+ let message="john";
+ let year =2019;
+
+ console.log(message);
+ console.log(message);
+ console.log(message);
+ console.log(message);
+ console.log(message);
+ console.log(message);
+ let name="John";
+ let value="admin";
+ console.log("john...");
+ //  alert("Hello World!...");
+
