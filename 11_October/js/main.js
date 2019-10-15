667c16f354446955b38bd19f17e1267ce07bc9f6
motokillishere <lolstilovskiy@gmail.com>
2019-10-15 17:10:53 +0400

    local changes

---
+++ 11_October/js/main.js
@@ -0,0 +1,45 @@
+let age= 24;
+console.log(age);
+
+
+age ="24";
+console.log(age)
+let checked =false; // true
+
+console.log(checked,typeof checked)
+
+// console.log (newAge >= age )
+// console.log (newAge <= age )
+
+/**
+ *
+ * user = { name: "",age: 24}
+ */
+const user =null;
+console.log(user)
+
+console.log(Number("2019"))
+console.log(String("2019"))
+console.log(Boolean("adsadsadsa"))
+console.log(Number(false))
+// alert("hello")
+// let userName = prompt("username: " );
+// alert (ur username - ${userName})
+
+// let sure = confirm("are u sure that u want delete?");
+// console.log(sure);
+prompt("username:")
+confirm("are u sure that u want to delete this?")
+console.log(sure);
+let counter = 3;
+// console.log(counter++)
+// console.log(counter)
+
+console.log(++counter)
+counter += 30;
+let num1=Number(prompt("write number 1"))
+let num2=Number(prompt("write number 2"))
+// num1=number(num1)
+// num2=number(num2)
+let sum=num1+num2
+console.log(sum);       
